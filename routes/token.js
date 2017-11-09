e7d5b49a144f324943128b61aca056d3436e679b
yrong <yrong1997@gmail.com>
2017-11-09 11:44:24 +0800

    token及相关用户信息不再写入cookie

--- routes/token.js
+++ routes/token.js
@@ -26,8 +26,6 @@ module.exports = (router)=>{
             user = _.omit(user,['passwd'])
             if(user.roles&&user.roles.length)
                 user.roles = await Role.mapRoles(user.roles)
-            ctx.cookies.set(common.TokenUserName, JSON.stringify(_.pick(user,['uuid','alias','userid','avatar','roles'])))
-            ctx.cookies.set(common.TokenName, token)
             ctx.body = {token: token,local:user}
         })(ctx, next)
     })
