815730089df86758d5342556c201ff6bdde315f5
yrong <yrong1997@gmail.com>
2018-04-26 09:29:03 +0800

    依赖升级＆schema变更＆搜索接口重构(3h)

--- search/index.js
+++ search/index.js
@@ -8,52 +8,48 @@ const es_client = new elasticsearch.Client({
 })
 const schema = require('redis-json-schema')
 const logger = require('log4js_wrapper').getLogger()
-const responseWrapper = require('../hook/responseWrapper')
+const responseHandler = require('../hook/responseWrapper')
+const hidden_fields = ['fields', 'cyphers', 'cypher', 'data', 'token', 'fields_old', 'change', '_id', '_index', '_type','user','id','method']
 
-const hidden_fields = ['fields', 'cyphers', 'cypher', 'data', 'token', 'fields_old', 'change', '_id', '_index', '_type','user','id']
-
-const addOrUpdateItem = async function(params, ctx) {
-    let schemas = schema.getSchemas(),category = schema.getAncestorCategory(params.category),index,index_obj
-    if(schemas[category]&&schemas[category].search){
-        index = schemas[category].search.index
-        index_obj = {
-            index: index,
-            type: 'doc',
-            refresh:true
-        }
-        if(!schemas[category].search.upsert){
-            index_obj.id = params.uuid
-        }
-        if(!ctx||ctx.method === 'POST'||schemas[category].search.upsert){
-            index_obj.body = _.omit(params,hidden_fields)
-            await es_client.index(index_obj)
-        }else if(ctx.method === 'PUT'||ctx.method === 'PATCH') {
-            index_obj.body = {doc: _.omit(params, hidden_fields)}
-            await es_client.update(index_obj)
-        }
-        logger.debug(`add index in es:${JSON.stringify(index_obj,null,'\t')}`)
+const addOrUpdateItem = async function(index,params,update) {
+    let index_obj,result
+    index_obj = {
+        index: index,
+        type: 'doc',
+        refresh:true
     }
+    index_obj.id = params.uuid
+    if(!update){
+        index_obj.body = _.omit(params,hidden_fields)
+        result = await es_client.index(index_obj)
+    }else{
+        index_obj.body = {doc: _.omit(params, hidden_fields)}
+        result = await es_client.update(index_obj)
+    }
+    logger.debug(`add index in es:${JSON.stringify({body:index_obj,result},null,'\t')}`)
 }
 
-const deleteItem = async function(params, ctx) {
-    let queryObj = params.uuid?{term:{uuid:params.uuid}}:{match_all:{}}
-    let schemas = schema.getSchemas(),category = params.category,index
-    if(ctx.deleteAll){
-        index = '*'
-    }else if((schemas[category]&&schemas[category].search)){
-        index = schemas[category].search.index
+const deleteItem = async function(index,params) {
+    let result,delObj = {
+        index: index,
+        body: {
+            query: {term:{uuid:params.uuid}}
+        },
+        refresh:true
     }
-    if(index){
-        let delObj = {
-            index: index,
-            body: {
-                query: queryObj
-            },
-            refresh:true
-        }
-        await es_client.deleteByQuery(delObj)
-        logger.debug(`delete index in es:${JSON.stringify(delObj,null,'\t')}`)
+    result  = await es_client.deleteByQuery(delObj)
+    logger.debug(`delete index in es:${JSON.stringify({body:delObj,result},null,'\t')}`)
+}
+
+const deleteAll = async function() {
+    let delObj = {
+        index: '*',
+        body: {
+            query: {match_all:{}}
+        },
+        refresh:true
     }
+    await es_client.deleteByQuery(delObj)
 }
 
 const searchItem = async (params, ctx)=> {
@@ -65,9 +61,13 @@ const searchItem = async (params, ctx)=> {
         params_pagination = {"from":from,"size":params.per_page}
     }
     let queryObj = params.body?{body:params.body}:{q:query}
-    let schemas = schema.getSchemas()
-    let category = params.category = params.category
-    let index = schemas[category].search.index
+    let index,schema_obj = schema.getAncestorSchema(params.category)
+    if(schema_obj&&schema_obj.search&&schema_obj.search.index){
+        index = schema_obj.search.index
+    }
+    if(!index){
+        throw new Error(`${params.category} not searchable`)
+    }
     if(queryObj.body&&queryObj.body.aggs){
         params_pagination = {size:0}
         params.aggs = true
@@ -76,10 +76,10 @@ const searchItem = async (params, ctx)=> {
         index: index,
         _source:_source
     },queryObj,params_pagination)
-    logger.debug(`search in es:${JSON.stringify(searchObj,null,'\t')}`)
-    let response = await es_client.search(searchObj)
-    response = await responseWrapper.esResponseMapper(response,params,ctx)
-    return response
+    let result = await es_client.search(searchObj)
+    logger.debug(`search in es:${JSON.stringify({body:searchObj,result},null,'\t')}`)
+    result = await responseHandler.esResponseMapper(result,params,ctx)
+    return result
 }
 
 const checkStatus = ()=> {
@@ -88,28 +88,29 @@ const checkStatus = ()=> {
     })
 }
 
-const batchUpdate = async (category,uuids,body)=>{
-    let bulks = [],schemas = schema.getSchemas()
-    category = schema.getAncestorCategory(category)
-    if(schemas[category]&&schemas[category].search) {
-        for (let uuid of uuids) {
-            bulks.push({update: {_index: schemas[category].search.index, _type: 'doc', _id: uuid}})
-            bulks.push(body)
-        }
-        await es_client.bulk({body: bulks, refresh: true})
+const batchUpdate = async (index,uuids,body)=>{
+    let bulks = [],result
+    for (let uuid of uuids) {
+        bulks.push({update: {_index: index, _type: 'doc', _id: uuid}})
+        bulks.push(body)
     }
+    result = await es_client.bulk({body: bulks, refresh: true})
+    logger.debug(`batch update index in es:${JSON.stringify({body:bulks,result},null,'\t')}`)
 }
 
-const batchCreate = async (category,items)=>{
-    let bulks = [],schemas = schema.getSchemas()
-    category = schema.getAncestorCategory(category)
-    if(schemas[category]&&schemas[category].search) {
-        for (let item of items) {
-            bulks.push({index: {_index: schemas[category].search.index, _type: 'doc', _id: item.uuid}})
-            bulks.push(item)
+const batchCreate = async (index,items,withoutId)=>{
+    let bulks = [],bulk_action,bulk_obj,result
+    for (let item of items) {
+        bulk_obj = _.omit(item,hidden_fields)
+        bulk_action = {_index: index, _type: 'doc', _id: item.uuid}
+        if(withoutId){
+            bulk_action = _.omit(bulk_action,['_id'])
         }
+        bulks.push({index:bulk_action})
+        bulks.push(bulk_obj)
     }
-    await es_client.bulk({body:bulks,refresh:true})
+    result = await es_client.bulk({body:bulks,refresh:true})
+    logger.debug(`batch add index in es:${JSON.stringify({body:bulks,result},null,'\t')}`)
 }
 
-module.exports = {searchItem,deleteItem,addOrUpdateItem,checkStatus,batchUpdate,batchCreate}
+module.exports = {searchItem,deleteItem,addOrUpdateItem,checkStatus,batchUpdate,batchCreate,deleteAll}
