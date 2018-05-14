b98df5548cf87e62e02f1f0b5027d8c88b5ec834
yrong <yrong1997@gmail.com>
2018-05-14 15:06:33 +0800

    角色的currentUserRole配合前端要求特殊处理(1h)

--- handlers/role.js
+++ handlers/role.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const acl = require('../lib/acl')
 const db = require('../lib/db')
 const ScirichonError = require('scirichon-common').ScirichonError
@@ -6,6 +7,7 @@ const preProcess = async (params, ctx)=>{
     if(ctx.method==='POST'||ctx.method==='PUT'||ctx.method==='PATCH') {
         params.type = params.fields.type = params.fields.type || 'internal'
         params.uuid = params.fields.uuid = params.fields.name
+        params.currentUserRole = params.fields.currentUserRole = params.fields.currentUserRole||[]
     }
     else if(ctx.method==='DELETE'){
         let cypher = `MATCH (n:User) where {uuid} in n.roles return n`
