3ac530baf5e14ce7c2102b688de9fc44eb3b27bc
yrong <yrong1997@gmail.com>
2018-06-03 10:33:57 +0800

    auth配合组件升级及api框架组件调整代码结构&&用例维护及测试(4.5h)
    启动方式参见package.json变更,"start": "scirichon-crud-api"
    通过scirichon-crud-api方式启动的服务需通过generateLicense脚本预先生成license

--- middleware/index.js
+++ middleware/index.js
@@ -4,17 +4,23 @@ const jwt_token = require('./token');
 const Redis = require('redis');
 const config = require('config')
 require('./passport')
+const compose = require('koa-compose')
 const passport = require('koa-passport')
+const session = require('koa-session')
 
-module.exports = function middleware() {
-    const redisOption = {host:`${process.env['REDIS_HOST']||config.get('redis.host')}`,port:config.get('redis.port')}
-    const redis_client = Redis.createClient(Object.assign({db:0},redisOption))
-    return [
-        passport.initialize(),
-        passport.session(),
-        jwt_token({
-            client: redis_client,
-            secret: config.get('secret')
-        }),
-    ]
+module.exports = {
+    load:(app)=>{
+        const redisOption = {host:`${process.env['REDIS_HOST']||config.get('redis.host')}`,port:config.get('redis.port')}
+        const redis_client = Redis.createClient(Object.assign({db:0},redisOption))
+        app.use(compose([
+            passport.initialize(),
+            passport.session(),
+            jwt_token({
+                client: redis_client,
+                secret: config.get('secret')
+            }),
+        ]));
+        app.keys = [config.get('secret')]
+        app.use(session({},app))
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -9,34 +9,27 @@ const scirichonSearch = require('scirichon-search')
 const userHandler = require('../handlers/user')
 const departmentHandler = require('../handlers/department')
 const roleHandler = require('../handlers/role')
-
-/**
- * legacy routes
- */
-router.post('/auth/hidden/clean', async(ctx, next) => {
-    await departmentHandler.clear()
-    await roleHandler.clear()
-    await userHandler.clear()
-    await scirichonCache.flushAll()
-    await scirichonSearch.deleteAll('role,department,user')
-    ctx.body = {}
-})
-router.use('/auth', authRoutes.routes(), authRoutes.allowedMethods())
-
-/**
- * new routes
- */
 const scirichonCrudHandler = require('scirichon-crud-handler')
-const crudRoutes = scirichonCrudHandler.routes
-const handlers = {'User':userHandler,'Department':departmentHandler,'Role':roleHandler}
-scirichonCrudHandler.hooks.setHandlers(handlers)
 
-const customizedRoutes = compose(
-        [
-            router.routes(),
-            router.allowedMethods()
-        ]
-)
+module.exports = {
+    load: (app)=>{
+        const handlers = {'User':userHandler,'Department':departmentHandler,'Role':roleHandler}
+        scirichonCrudHandler.hooks.setHandlers(handlers)
+        scirichonCrudHandler.routes(app)
+        router.post('/auth/hidden/clean', async(ctx, next) => {
+            await departmentHandler.clear()
+            await roleHandler.clear()
+            await userHandler.clear()
+            await scirichonCache.flushAll()
+            await scirichonSearch.deleteAll('role,department,user')
+            ctx.body = {}
+        })
+        router.use('/auth', authRoutes.routes(), authRoutes.allowedMethods())
+        app.use(compose(
+            [
+                router.routes(),
+                router.allowedMethods()
+            ]))
+    }
+}
 
-
-module.exports = {crudRoutes,customizedRoutes}
