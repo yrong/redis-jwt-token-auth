884f11c82a52d33f344b2d8e146b306b7839ab2d
snail <snail_88@126.com>
2018-11-05 11:42:23 +0800

    登录接口过滤roles,departments对象状态为deleted和disabled的数据

--- routes/DataServiceImpl.js
+++ routes/DataServiceImpl.js
@@ -1,5 +1,10 @@
 const _ = require('lodash')
 const handler = require('../handlers/index')
+const passport = require('koa-passport')
+const LdapAccount = require('../handlers/ldap_account')
+const ScirichonError = require('scirichon-common').ScirichonError
+const user_handler = require('../handlers/user')
+const TokenExpiration = require('../lib/const').TokenExpiration
 
 module.exports = (router) =>{
 
@@ -31,6 +36,32 @@ module.exports = (router) =>{
             }
         }
         ctx.body = {data: department}
+    })
+
+    router.post('/login/filter', async(ctx, next) => {
+        let token,local
+        await passport.authenticate('local',async(err,user) => {
+            if(err){
+                ctx.throw(new ScirichonError(err.message,401))
+            }
+            local = await user_handler.checkUser(ctx,user)
+            let roles = local.roles||[];
+            for(let i = 0;i<roles.length;i++){
+                if(roles[i].status === 'deleted' || roles[i].status === 'disabled'){
+                    roles.splice(i,1);
+                }
+            }
+            let departments = local.departments||[];
+            for(let j = 0;j<departments.length;j++){
+                if(departments[j].status === 'deleted' || departments[j].status === 'disabled'){
+                    departments.splice(j,1);
+                }
+            }
+            await ctx.login(user)
+            token = await ctx.req.session.create(ctx.req.session.passport)
+
+            ctx.body = {token: token,login_date:new Date().toISOString(),expiration_date:new Date(Date.now()+TokenExpiration*1000).toISOString(),local:local}
+        })(ctx, next)
     })
 
 }
