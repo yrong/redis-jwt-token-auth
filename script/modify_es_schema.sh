dd4b5cec07bbae83ab45674e946400ac977ae807
yrong <yrong1997@gmail.com>
2018-11-23 11:07:40 +0800

    支持拼音分词组件及脚本升级&用例维护与测试(4h)
    支持拼音分词及相关字段排序步骤:
    1,根据es版本下载对应拼音分词插件版本https://github.com/medcl/elasticsearch-analysis-pinyin/解压到es插件目录
    2,重建支持拼音分词的索引,npm run initESWithPinyin(以新增字段名test_pinyin为例子)

--- script/modify_es_schema.sh
+++ script/modify_es_schema.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-curl -XPUT localhost:9200/user/_mapping/doc -d '
+curl --header "Content-Type: application/json" -XPUT localhost:9200/user/_mapping/doc -d'
     {
             "properties" : {
-                "test_expiration_date" : {"type" : "keyword"}
+                "test_expiration_date" : {"type" : "keyword"},
+                "test_pinyin":
+                {
+                     "type" : "text",
+                     "analyzer" : "pinyin_analyzer",
+                     "fielddata": true
+                }
             }
     }
 '
