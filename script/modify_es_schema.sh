c51b3b149b9f876d98f3846a240e8479d9e2792a
yrong <yrong1997@gmail.com>
2018-12-10 13:58:42 +0800

    auth配合crud组件升级＆用例维护及测试(3h)
    crud组件升级
    增加batch delete相关用例及测试
    拼音分词通过动态模板配置，不再需要显式维护mapping(字段后缀为_pinyin自动使用拼音分词配置）

--- script/modify_es_schema.sh
+++ script/modify_es_schema.sh
@@ -3,13 +3,7 @@
 curl --header "Content-Type: application/json" -XPUT localhost:9200/user/_mapping/doc -d'
     {
             "properties" : {
-                "test_expiration_date" : {"type" : "keyword"},
-                "test_pinyin":
-                {
-                     "type" : "text",
-                     "analyzer" : "pinyin_analyzer",
-                     "fielddata": true
-                }
+                "test_expiration_date" : {"type" : "keyword"}
             }
     }
 '
